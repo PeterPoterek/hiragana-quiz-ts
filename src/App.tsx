daf0e06532ca3056e7f719a1673b3a72bc8fdfb1
Piotr Poterek <peterpoterek0@gmail.com>
2023-10-17 23:20:52 +0200

    Buttons ( work in progress )

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import hiraganas from "./hiraganas";
 import "./App.css";
 
 function App() {
-  const [currentHiragana, setCurrentHiragana] = useState("");
+  const [currentHiragana, setCurrentHiragana] = useState<{ id: number; character: string } | null>(null);
+  const [buttons, setButtons] = useState<string[]>([]);
 
   const getRandomHiragana = () => {
     const keys = Object.keys(hiraganas);
     const randomKey = keys[Math.floor(Math.random() * keys.length)];
 
-    setCurrentHiragana(hiraganas[randomKey].character);
+    setCurrentHiragana(hiraganas[randomKey]);
+  };
+
+  const getButtons = () => {
+    const keys = Object.keys(hiraganas);
+    const randomIndexes = Array.from({ length: 3 }, () => Math.floor(Math.random() * keys.length));
+    const randomButtonKeys = randomIndexes.map((index) => keys[index]);
+    setButtons(randomButtonKeys);
   };
 
   useEffect(() => {
     getRandomHiragana();
+    getButtons();
   }, []);
 
   return (
     <>
-      <h1>{currentHiragana}</h1>
-
+      <h1>{currentHiragana?.character}</h1>
       <div>
-        <button>E</button>
-        <button>A</button>
-        <button>O </button>
+        {buttons.map((item, index) => (
+          <button key={index}>{item}</button>
+        ))}
       </div>
     </>
   );
